253bb4c3a796d80204b4fdc5b1e352bc48009162
JJ-quadour <96658466+Jawdat-Kadour@users.noreply.github.com>
2025-08-31 14:35:01 +0300

    changed font, added volunteering

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,30 +17,30 @@ module.exports = {
         mono: ["Share Tech Mono", "monospace"], // Monospaced for a techy feel
       },
       animation: {
-        'fade-in-down': 'fadeInDown 0.5s ease-out forwards',
-        'fade-in-up': 'fadeInUp 0.5s ease-out forwards',
-        'fade-in-section': 'fadeIn 1s ease-out forwards',
-        'pulse-glow': 'pulseGlow 2s infinite ease-in-out',
+        "fade-in-down": "fadeInDown 0.5s ease-out forwards",
+        "fade-in-up": "fadeInUp 0.5s ease-out forwards",
+        "fade-in-section": "fadeIn 1s ease-out forwards",
+        "pulse-glow": "pulseGlow 2s infinite ease-in-out",
       },
       keyframes: {
         fadeInDown: {
-          '0%': { opacity: '0', transform: 'translateY(-20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          "0%": { opacity: "0", transform: "translateY(-20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
         fadeInUp: {
-          '0%': { opacity: '0', transform: 'translateY(20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
         pulseGlow: {
-          '0%, 100%': { boxShadow: '0 0 5px #00DCFF, 0 0 10px #00DCFF' },
-          '50%': { boxShadow: '0 0 20px #00DCFF, 0 0 30px #00DCFF' },
-        }
-      }
+          "0%, 100%": { boxShadow: "0 0 5px #00DCFF, 0 0 10px #00DCFF" },
+          "50%": { boxShadow: "0 0 20px #00DCFF, 0 0 30px #00DCFF" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
